34c7bfb588c12ec94304e96ea094bdf0824f05a1
liuhuapiaoyuan <278780765@qq.com>
2024-11-15 13:16:29 +0800

    refactor: update UploadFileCard icon prop type and handling

--- app/example/src/components/upload/upload-avatar-handler.tsx
+++ app/example/src/components/upload/upload-avatar-handler.tsx
@@ -14,7 +14,7 @@ export interface UploadFileCardProps {
   /**
    * 图标
    */
-  icon?: any
+  icon?: string
   /**
    * 文件大小
    */
@@ -47,9 +47,11 @@ export function UploadFileCard({
         <div
           className='w-full h-full bg-contain rounded-xl'
           style={
-            icon && {
-              backgroundImage: `url(${icon})`,
-            }
+            icon
+              ? {
+                  backgroundImage: `url(${icon})`,
+                }
+              : {}
           }
         ></div>
         <div className='absolute z-20 right-0 top-0 p-2'>
